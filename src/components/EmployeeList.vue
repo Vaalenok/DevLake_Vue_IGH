b6118b02b5b7043627660a481d0426fb553e1f76
AleksRiffel <avr-tum@mail.ru>
2024-11-03 23:11:53 +0500

    Update EmployeeList.vue

--- src/components/EmployeeList.vue
+++ src/components/EmployeeList.vue
@@ -3,17 +3,11 @@
 
   <h1 style="margin-bottom: 10px">Сотрудники</h1>
 
-  <el-input style="font-size: 18px" v-model="search" size="large" placeholder="Поиск по имени">
-    <template #prepend>
-      <el-icon class="el-input__icon"><Search /></el-icon>
-    </template>
-  </el-input>
-
   <el-table
       stripe
       row-key="name"
       :size="'large'"
-      :data="filterTableData"
+      :data="tableData"
       style="width: 100%; font-size: 16px"
       @row-click="handleRowClick"
   >
@@ -21,13 +15,11 @@
         prop="name"
         label="ФИО"
         sortable
-        width="200"
     />
     <el-table-column
         prop="company"
         label="Компания"
         sortable
-        width="300"
     />
     <el-table-column
         prop="experience"
@@ -38,7 +30,6 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 import { getAllUsers } from '/src/scripts/requests.js'
 import { ref, computed } from 'vue'
@@ -50,15 +41,6 @@ interface User {
   company: string
   experience: number
 }
-
-const search = ref('')
-const filterTableData = computed(() =>
-    tableData.filter(
-        (data) =>
-            !search.value ||
-            data.name.toLowerCase().includes(search.value.toLowerCase())
-    )
-)
 
 const router = useRouter()
 const tableData = ref<User[]>([]) // Инициализируем как реактивную переменную
