66b57c253e609261e6bbf9d443359237b5f8a238
AleksRiffel <avr-tum@mail.ru>
2024-11-03 22:10:18 +0500

    Update pages

--- src/components/EmployeeList.vue
+++ src/components/EmployeeList.vue
@@ -1,8 +1,20 @@
 <template>
+  <TopBar/>
+
+  <h1 style="margin-bottom: 10px">Сотрудники</h1>
+
+  <el-input style="font-size: 18px" v-model="search" size="large" placeholder="Поиск по имени">
+    <template #prepend>
+      <el-icon class="el-input__icon"><Search /></el-icon>
+    </template>
+  </el-input>
+
   <el-table
+      stripe
       row-key="name"
-      :data="tableData"
-      style="width: 100%"
+      :size="'large'"
+      :data="filterTableData"
+      style="width: 100%; font-size: 16px"
       @row-click="handleRowClick"
   >
     <el-table-column
@@ -22,13 +34,14 @@
         label="Стаж"
         sortable
     />
-
   </el-table>
 </template>
 
 <script lang="ts" setup>
 import { useRouter } from 'vue-router'
 import { getAllUsers } from '/src/scripts/requests.js'
+import { ref, computed } from 'vue'
+import TopBar from "@/components/TopBar.vue";
 
 interface User {
   id: number
@@ -36,6 +49,15 @@ interface User {
   company: string
   experience: number
 }
+
+const search = ref('')
+const filterTableData = computed(() =>
+    tableData.filter(
+        (data) =>
+            !search.value ||
+            data.name.toLowerCase().includes(search.value.toLowerCase())
+    )
+)
 
 const router = useRouter()
 
---
+++ src/components/TopBar.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import { useRouter } from 'vue-router'
+
+  const router = useRouter()
+</script>
+
+<template>
+  <el-menu class="el-menu-demo" style="align-items: center; margin-bottom: 10px" mode="horizontal">
+    <el-page-header @back="router.push('/')" title="Назад">
+      <template #content>
+        <span class="text-large font-600 mr-3"> ScoreWorker </span>
+      </template>
+    </el-page-header>
+  </el-menu>
+</template>
