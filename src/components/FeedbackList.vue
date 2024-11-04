e95dcadb0dc8e5385cf70104dfcd1e4ce439daaf
Pankyxa <kbelozerov1@gmail.com>
2024-11-04 06:35:13 +0300

    Refactor

--- src/components/FeedbackList.vue
+++ src/components/FeedbackList.vue
@@ -11,16 +11,16 @@
         v-if="totalPages > 1"
         background
         layout="prev, pager, next"
-        :total="feedbacks.length"
+        :total="totalFeedbacks"
         :page-size="pageSize"
-        v-model:current-page="currentPage"
+        :current-page.sync="currentPage"
         @current-change="handlePageChange"
     />
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref, computed, onMounted } from 'vue';
+import { ref, computed, onMounted, watch } from 'vue';
 import FeedbackCard from '@/components/FeedbackCard.vue';
 import { getFeedback } from '@/scripts/requests';
 import { defineProps } from 'vue';
@@ -28,35 +28,40 @@ import { defineProps } from 'vue';
 // Определение пропсов
 const props = defineProps<{ userId: string }>();
 
+// Состояние компонентов
 const feedbacks = ref([]);
+const totalFeedbacks = ref(0);
 const pageSize = 5;  // Количество элементов на одной странице
 const currentPage = ref(1);
 
+// Функция для получения отзывов
 const fetchFeedbacks = async () => {
   try {
-    console.log(props.userId);
-    feedbacks.value = await getFeedback(props.userId);
+    const { data, total } = await getFeedback(props.userId, (currentPage.value - 1) * pageSize, pageSize);
+    feedbacks.value = data;
+    totalFeedbacks.value = total;
   } catch (error) {
     console.error("Ошибка при получении отзывов:", error);
   }
 };
 
-// Определяем общее количество страниц на основе количества отзывов
-const totalPages = computed(() => Math.ceil(feedbacks.value.length / pageSize));
-
 // Пагинация — выводим только отзывы для текущей страницы
-const paginatedFeedbacks = computed(() => {
-  const startIndex = (currentPage.value - 1) * pageSize;
-  const endIndex = startIndex + pageSize;
-  return feedbacks.value.slice(startIndex, endIndex);
-});
+const paginatedFeedbacks = computed(() => feedbacks.value);
 
+// Определяем общее количество страниц
+const totalPages = computed(() => Math.ceil(totalFeedbacks.value / pageSize));
+
+// Обработчик изменения страницы
 const handlePageChange = (page: number) => {
   currentPage.value = page;
+  fetchFeedbacks(); // заново загружаем данные для новой страницы
 };
 
-// Используем userId, полученный через props
-onMounted(fetchFeedbacks);
+// Следим за изменением currentPage и загружаем новые данные
+watch(currentPage, () => fetchFeedbacks());
+
+// Инициализируем загружаемые данные
+onMounted(() => fetchFeedbacks());
 </script>
 
 <style scoped>
